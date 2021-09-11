392c492726cdc71550a70dfe8b8f3693c1b7a3ac
AlenkinAndrew <oxqq@ya.ru>
2021-09-11 17:22:41 +0800

    enable_caching

--- src/main/java/edu/alenkin/topjavagraduation/TopjavaGraduationApplication.java
+++ src/main/java/edu/alenkin/topjavagraduation/TopjavaGraduationApplication.java
@@ -2,6 +2,7 @@ package edu.alenkin.topjavagraduation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableCaching
 public class TopjavaGraduationApplication {
 
     public static void main(String[] args) {
